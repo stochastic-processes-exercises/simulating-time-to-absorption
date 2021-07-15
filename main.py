ca8555d7bb58fbabe9ef18c5283561f992d78f29
Gareth Tribello <gareth.tribello@gmail.com>
2021-07-15 14:16:08 +0100

    Built tests for exercise

---
+++ main.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def markov_move( trans, start ) :
+
+
+
+def nsteps_to_absorption( trans, start ) :
+
+
+# Setup the transition matrix here
+A = 
---
+++ testing/test_main.py
@@ -0,0 +1,29 @@
+try:
+    from AutoFeedback.plotchecks import check_plot
+except:
+    import subprocess
+    import sys
+
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "AutoFeedback"])
+    from AutoFeedback.plotchecks import check_plot
+
+from AutoFeedback.plotclass import line
+import unittest
+from main import *
+
+S, S2, myp = 0, 0, np.array([[1,0,0,0,0],[1/3,1/3,1/3,0,0],[0,0.5,0,0.5,0],[0,0.5,0,0,0.5],[0,0,0,0,1]]) 
+for i in range(100) : 
+    var = nsteps_to_absorption( myp, 2 )
+    S, S2 = S + var, S + var*var
+mean = S / 100
+var = (100/99)*( S2/100 - mean ) 
+
+myq = np.array([[1/3,1/3,0],[0.5,0,0.5],[0.5,0,0]])
+my_inv = np.linalg.inv( np.identity(3) - myq )
+rand = randomvar( np.dot( my_inv, np.array([1,1,1]) )[0], variance=var, vmin=1, isinteger=True )
+line1 = line( np.linspace(1,100,100),  rand )  )
+axislabels=["Index", "Number of steps till absorption"]
+
+class UnitTests(unittest.TestCase) :
+    def test_plot(self) :
+        assert(check_plot([line1],explabels=axislabels,explegend=False,output=True))
