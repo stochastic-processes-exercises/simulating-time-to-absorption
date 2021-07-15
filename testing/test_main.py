7238445550419271952ba511a80761b309e39226
Gareth Tribello <gareth.tribello@gmail.com>
2021-07-15 14:25:16 +0100

    Fixed tests

--- testing/test_main.py
+++ testing/test_main.py
@@ -8,12 +8,13 @@ except:
     from AutoFeedback.plotchecks import check_plot
 
 from AutoFeedback.plotclass import line
+from AutoFeedback.randomclass import randomvar
 import unittest
 from main import *
 
 S, S2, myp = 0, 0, np.array([[1,0,0,0,0],[1/3,1/3,1/3,0,0],[0,0.5,0,0.5,0],[0,0.5,0,0,0.5],[0,0,0,0,1]]) 
 for i in range(100) : 
-    var = nsteps_to_absorption( myp, 2 )
+    var = nsteps_to_absorption( myp, 1 )
     S, S2 = S + var, S + var*var
 mean = S / 100
 var = (100/99)*( S2/100 - mean ) 
@@ -21,7 +22,7 @@ var = (100/99)*( S2/100 - mean )
 myq = np.array([[1/3,1/3,0],[0.5,0,0.5],[0.5,0,0]])
 my_inv = np.linalg.inv( np.identity(3) - myq )
 rand = randomvar( np.dot( my_inv, np.array([1,1,1]) )[0], variance=var, vmin=1, isinteger=True )
-line1 = line( np.linspace(1,100,100),  rand )  )
+line1 = line( np.linspace(1,20,20),  rand ) 
 axislabels=["Index", "Number of steps till absorption"]
 
 class UnitTests(unittest.TestCase) :
