eca447243c8b2d44b89d879a847710b33a795f8d
Gareth Tribello <gareth.tribello@gmail.com>
2022-08-16 16:26:25 +0100

    Fixed small bug

--- testing/test_main.py
+++ testing/test_main.py
@@ -15,9 +15,9 @@ from main import *
 S, S2, myp = 0, 0, np.array([[1,0,0,0,0],[1/3,1/3,1/3,0,0],[0,0.5,0,0.5,0],[0,0.5,0,0,0.5],[0,0,0,0,1]]) 
 for i in range(100) : 
     var = nsteps_to_absorption( myp, 1 )
-    S, S2 = S + var, S + var*var
+    S, S2 = S + var, S2 + var*var
 mean = S / 100
-var = (100/99)*( S2/100 - mean ) 
+var = (100/99)*( S2/100 - mean*mean ) 
 
 myq = np.array([[1/3,1/3,0],[0.5,0,0.5],[0.5,0,0]])
 my_inv = np.linalg.inv( np.identity(3) - myq )
